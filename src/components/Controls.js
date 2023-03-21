290266dc1aca16385a094861a84e2e313e2a2e9e
alexdietz1988 <alex.dietz@gmail.com>
2023-03-21 02:39:21 -0400

    Use object destructuring for Controls props

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,31 +1,51 @@
 import { DateControls } from "./DateControls"
 
 export const Controls = ({ settings, settingsHandlers, data }) => {
+    const { 
+        mediaType, 
+        media, 
+        genre, 
+        view, 
+        selectedCreator, 
+        selectedWork, 
+        specialNames 
+    } = settings;
+    const { 
+        restoreDefaults, 
+        setMediaType, 
+        setMedia, 
+        setGenre, 
+        setSelectedCreator, 
+        setSelectedWork,
+        setDisplayLimit, 
+        setView
+    } = settingsHandlers;
+
     return(
         <section className='section'>
             <div className='m-2'>
                 <button
-                    className={settings.mediaType === 'literature' ? 'button is-primary' : 'button'}
-                    onClick={() => {settingsHandlers.restoreDefaults(); settingsHandlers.setMediaType('literature');}}>
+                    className={mediaType === 'literature' ? 'button is-primary' : 'button'}
+                    onClick={() => {restoreDefaults(); setMediaType('literature');}}>
                     Literature
                 </button>
                 <button
-                    className={settings.mediaType === 'music' ? 'button is-primary' : 'button'}
-                    onClick={() => {settingsHandlers.restoreDefaults(); settingsHandlers.setMediaType('music');}}>
+                    className={mediaType === 'music' ? 'button is-primary' : 'button'}
+                    onClick={() => {restoreDefaults(); setMediaType('music');}}>
                     Music
                 </button>
             </div>
-            {settings.mediaType === 'literature' 
+            {mediaType === 'literature' 
                 ? (
                     <div className='m-2'>
                         <button
-                            className={settings.genre === 'fiction' ? 'button is-primary' : 'button'}
-                            onClick={() => {settingsHandlers.restoreDefaults(); settingsHandlers.setGenre('fiction'); }}>
+                            className={genre === 'fiction' ? 'button is-primary' : 'button'}
+                            onClick={() => {restoreDefaults(); setGenre('fiction'); }}>
                             Fiction
                         </button>
                         <button
-                            className={settings.genre === 'nonfiction' ? 'button is-primary' : 'button'}
-                            onClick={() => {settingsHandlers.restoreDefaults(); settingsHandlers.setGenre('nonfiction');}}>
+                            className={genre === 'nonfiction' ? 'button is-primary' : 'button'}
+                            onClick={() => {restoreDefaults(); setGenre('nonfiction');}}>
                             Nonfiction
                         </button>
                     </div>
@@ -33,14 +53,14 @@ export const Controls = ({ settings, settingsHandlers, data }) => {
                 : null}
             <div className='m-2'>
                 <button
-                    className={settings.media === 'works' ? 'button is-primary' : 'button'}
-                    onClick={() => {settingsHandlers.restoreDefaults(); settingsHandlers.setMedia('works');}}>
-                    {settings.specialNames.works[0].toUpperCase() + settings.specialNames.works.slice(1)}
+                    className={media === 'works' ? 'button is-primary' : 'button'}
+                    onClick={() => {restoreDefaults(); setMedia('works');}}>
+                    {specialNames.works[0].toUpperCase() + specialNames.works.slice(1)}
                 </button>
                 <button
-                    className={settings.media === 'creators' ? 'button is-primary' : 'button'}
-                    onClick={() => {settingsHandlers.restoreDefaults(); settingsHandlers.setMedia('creators');}}>
-                    {settings.specialNames.creators[0].toUpperCase() + settings.specialNames.creators.slice(1)}
+                    className={media === 'creators' ? 'button is-primary' : 'button'}
+                    onClick={() => {restoreDefaults(); setMedia('creators');}}>
+                    {specialNames.creators[0].toUpperCase() + specialNames.creators.slice(1)}
                 </button>
             </div>
             <DateControls
@@ -49,27 +69,27 @@ export const Controls = ({ settings, settingsHandlers, data }) => {
                 data={data} />
             <div className='m-2'>
                 <button
-                    className={settings.view === 'table' ? 'button is-primary' : 'button'}
-                    onClick={() => {settingsHandlers.setView('table'); settingsHandlers.setDisplayLimit(25);}}>
+                    className={view === 'table' ? 'button is-primary' : 'button'}
+                    onClick={() => {setView('table'); setDisplayLimit(25);}}>
                     Table View
                 </button>
                 <button
-                    className={settings.view === 'compact' ? 'button is-primary' : 'button'}
-                    onClick={() => {settingsHandlers.setView('compact'); settingsHandlers.setDisplayLimit(50);}}>
+                    className={view === 'compact' ? 'button is-primary' : 'button'}
+                    onClick={() => {setView('compact'); setDisplayLimit(50);}}>
                     Compact View
                 </button>
             </div>
-            { settings.selectedCreator
+            { selectedCreator
             ? (
-                <button className='tag m-2' onClick={() => settingsHandlers.setSelectedCreator('')}>
-                    {settings.selectedCreator}
+                <button className='tag m-2' onClick={() => setSelectedCreator('')}>
+                    {selectedCreator}
                 </button>
             )
             : null}
-            { settings.selectedWork
+            { selectedWork
             ? (
-                <button className='tag m-2' onClick={() => settingsHandlers.setSelectedWork('')}>
-                    {settings.selectedWork}
+                <button className='tag m-2' onClick={() => setSelectedWork('')}>
+                    {selectedWork}
                 </button>
             )
             : null}
