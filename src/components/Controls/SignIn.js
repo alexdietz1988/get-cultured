6f794580f93afbda45c21761de6b92dba0094bfc
alexdietz1988 <alex.dietz@gmail.com>
2023-05-20 05:51:20 -0400

    Tweak SignIn

--- src/components/Controls/SignIn.js
+++ src/components/Controls/SignIn.js
@@ -4,8 +4,7 @@ export const SignIn = ({ userData }) => {
     const { userId, setUserId, backend } = userData;
     const [userInput, setUserInput] = useState('');
     const [passwordInput, setPasswordInput] = useState('');
-    const [signinType, setSigninType] = useState('login');
-    const postCredentials = async () => {
+    const postCredentials = async (signinType) => {
         const response = signinType === 'login' 
             ? await backend.post('auth/login', { 
                 user: userInput, 
@@ -23,26 +22,26 @@ export const SignIn = ({ userData }) => {
         ? <button className='button is-warning' onClick={() => setUserId('')}>Logout</button>
         : (
         <>
-            <button className='button is-danger mb-3' onClick={() => setUserId('642da0fb02e85e64261c13c6')}>Quick Sign In (Developer Mode)</button>
             <form className='form control' onSubmit={e => {
                 e.preventDefault();
                 postCredentials();
                 }}>
-                <input id='user' className='input is-inline mr-3' placeholder='Username' 
-                    onChange={e => setUserInput(e.target.value)}/>
-                <input id='password' type='password' className='input is-inline mr-3' placeholder='Password' 
-                    onChange={e => setPasswordInput(e.target.value)}/>
-                <div className='buttons is-inline has-addons mr-3'>
-                    <button 
-                        className={'button ' + (signinType === 'login' && 'is-primary')}
-                        onClick={() => setSigninType('login')}>
-                        Login</button>
-                    <button 
-                        className={'button ' + (signinType === 'signup' && 'is-primary')}
-                        onClick={() => setSigninType('signup')}>
-                        Sign Up</button>
+                <div className='mb-2'>
+                    <input id='user' className='input is-inline mr-3' placeholder='Username' 
+                        onChange={e => setUserInput(e.target.value)}/>
+                    <input id='password' type='password' className='input is-inline' placeholder='Password' 
+                        onChange={e => setPasswordInput(e.target.value)}/>
                 </div>
-                <button type='submit is-inline' className='button'>Submit</button>
+                <div className='buttons'>
+                    <div 
+                        className='button'
+                        onClick={() => postCredentials('login')}>
+                        Login</div>
+                    <div 
+                        className='button'
+                        onClick={() => postCredentials('signup')}>
+                        Sign Up</div>
+                </div>
             </form>
         </>
     );
